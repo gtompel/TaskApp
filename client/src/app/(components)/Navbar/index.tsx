49ea09a08b990c76be3d5dc4eeec61a057f562bd
gtompel <quimpik@yandex.ru>
2024-11-06 18:35:51 +0300

    28:01

---
+++ client/src/app/(components)/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between bg-white px-4'>
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar
