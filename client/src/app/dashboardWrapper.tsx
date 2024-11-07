79ae8f61b227347235fb399a112a278ccc3de043
gtompel <quimpik@yandex.ru>
2024-11-07 18:44:36 +0300

    1:04

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider from './redux';
 
-const DashboardWrapper = ({ children }:{children:React.ReactNode}) => {
+const DashboardLayout = ({ children }:{children:React.ReactNode}) => {
   return (
     <div className='flex in-h-screen w-full bg-gray-50 text-gray-900'>
         {/* Sidebar */}
+        <Sidebar />
         <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
         {/* Navbar*/ }
         <Navbar />
         {children}
         </main>
     </div>
-  )
-}
+  );
+};
+
+const DashboardWrapper = ({ children }:{children:React.ReactNode}) => {
+  return (
+    <StoreProvider>
+    <DashboardLayout>
+      {children}
+    </DashboardLayout>
+    </StoreProvider>  
+  );
+};
 
 export default DashboardWrapper
